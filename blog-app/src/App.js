ad2014f5977fdc49cfecb4dc4e725fdbd8034873
SaraMecinovic1 <saramecinovic87@gmail.com>
2023-06-03 15:47:36 +0200

    react-router-dom

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Blogs from "./pages/Blogs";
+import BlogDetails from "./pages/BlogDetails";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/details/:id" element={<BlogDetails />} />
+        <Route path="/helloworld" element={<div>Hello world</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ blog-app/src/pages/Blogs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Blogs() {
+  const location = useLocation();
+
+  console.log(location.state);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <div style={{ border: "1px solid black" }}>
+        <h3>This is my blog title</h3>
+        <p>This blog is about programming</p>
+        <Link  // kao <a> tag za link
+          to={"/details/1234"}  // gde zelimo da nas prebaci
+          state={{
+            firstName: "Muhamed",
+            lastName: "Muminovic",
+            location: { city: "Novi Pazar", country: "Serbia" },
+          }}
+        >
+          Go to details
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Blogs;
