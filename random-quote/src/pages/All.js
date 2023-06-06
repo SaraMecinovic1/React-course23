0c5f48ccb8c0da1b40214512775c7b61691b5d49
SaraMecinovic1 <saramecinovic87@gmail.com>
2023-06-06 23:58:06 +0200

    new

--- random-quote/src/pages/All.js
+++ random-quote/src/pages/All.js
@@ -1,28 +1,66 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const All = () => {
   const [state, setState] = useState([]);
+  const navigate = useNavigate;
 
-  useEffect 
-    (() => {
-      fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
-        .then((res) => res.json())
-        .then((data) => {
-          setState(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    []);
+const goTo=(props)=>{
+  navigate("/", + props , "")
+}
+
+  useEffect(() => {
+    fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   console.log(state);
 
-  return <div className="all-quotes">
-   {state.map((quote,index)=>{
-return(
-  <p> <i>{}</i></p>
-)
-   })}
-  </div>;
+  return (
+    <div className="all-quotes">
+      <div>
+        {state.map((quote, index) => {
+          return (
+           <div className="card"> 
+            
+              <h3>
+               
+                <i>{quote.quoteText}</i>
+              </h3>
+            
+            <p>{quote.quoteAuthor}</p>
+            <p>{quote.quoteSource}</p>
+            
+              <div className="goto">
+                <button
+                  onClick={() => {
+                   goTo(quote._id)
+                   console.log(quote._id , "-----")
+                  }}
+                >
+                  Details
+                </button>
+
+                <button
+                  onClick={() => {
+                    // navigate("/details/qoute_id");
+                    console.log("likes");
+                  }}
+                >
+                  Likes
+                </button>
+           </div>
+              </div>
+            
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default All;
