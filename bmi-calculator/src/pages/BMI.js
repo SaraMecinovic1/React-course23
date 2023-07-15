438824b0dc4d391308da1fe75346ac47377fc2d8
Sara Mecinovic <saramecinovic87@gmail.com>
2023-07-15 22:51:54 +0200

    bmi calculator

---
+++ bmi-calculator/src/pages/BMI.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+
+
+function BMI() {
+  return (
+    <div className='page1' >
+     <div className='card'>
+        <div className='personIcon'></div>
+        <div className='info'></div>
+     </div>
+    </div>
+  )
+}
+
+export default BMI
