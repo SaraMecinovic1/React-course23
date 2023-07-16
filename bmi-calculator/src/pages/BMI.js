35a909bd62d0e5487669aeb853ca181f6dc85722
Sara Mecinovic <saramecinovic87@gmail.com>
2023-07-16 21:55:44 +0200

    bmi calculator

--- bmi-calculator/src/pages/BMI.js
+++ bmi-calculator/src/pages/BMI.js
@@ -1,32 +1,115 @@
 import React from "react";
 import "../App.css";
-import Muskarac from "../character-is-man-businessman-stands-looks-forward/217.jpg";
-import { Zena } from "../young-woman-standing-white/39538.jpg";
+import Muskarac from "../muskarac/217.jpg";
 import { Female, Male } from "@mui/icons-material";
-import Button from '@mui/material/Button';
+import girl from "../zena/39538.jpg";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
 
 function BMI() {
+  const [selectedGender, setSelectedGender] = useState("");
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [bmi, setBmi] = useState("");
+  const [description, setDescription] = useState("");
+
+  const calculateBMI = (event) => {
+    if (weight === 0 || weight === 0) {
+      alert("Please enter valid weight and weight ");
+    } else {
+      const bmi = weight / ((height / 100) * (height / 100));
+      setBmi(bmi);
+    }
+
+    if (bmi < 25) {
+      setDescription("You are underweight");
+    }
+    if (bmi >= 25 && bmi < 30) {
+      setDescription("You are a healthy weight");
+    } else {
+      setDescription("You are overweight");
+    }
+  };
+  console.log(bmi);
+  console.log(description);
+  const handleGenderClick = (gender) => {
+    setSelectedGender(gender);
+  };
+
   return (
     <div className="page1">
       <div className="card">
         <div className="personIcon">
-          <img src={Muskarac} alt="Moja slika" width={200} height={"85%"}></img>
+          {/* <img src={Muskarac} alt="Moja slika" width={200} height={"83%"}></img> */}
+          {selectedGender === "female" && (
+            <img
+              className="zena"
+              src={girl}
+              alt="Moja slika"
+              width={200}
+              height={"77%"}></img>
+          )}
+          {selectedGender === "male" && (
+            <img
+              src={Muskarac}
+              alt="Moja slika"
+              width={200}
+              height={"83%"}></img>
+          )}
 
-          <Button sx={{width:250, height:45 , marginTop:3, marginLeft:35,backgroundColor:"rgb(84, 176, 215)" ,"&:hover": {
-      backgroundColor: "rgb(50, 120, 160)",
-    },}}  variant="contained">Calculate</Button>
-
+          <Button
+            sx={{
+              width: 250,
+              height: 45,
+              marginTop: 3,
+              marginLeft: 35,
+              backgroundColor: "rgb(84, 176, 215)",
+              "&:hover": {
+                backgroundColor: "rgb(50, 120, 160)",
+              },
+            }} onClick={()=> calculateBMI()}
+            variant="contained">
+            Calculate
+          </Button>
         </div>
         <div className="info">
           <div className="gender">
             <h3>Gender:</h3>
             <div className="genders">
-              <Female
-                sx={{ border: 1, fontSize: 42, color: " rgb(84, 176, 215)" }}
-              />
-              <Male
-                sx={{ border: 1, fontSize: 42, color: " rgb(84, 176, 215)" }}
-              />
+              <IconButton
+                id="girl"
+                onClick={() => handleGenderClick("female")}
+                sx={{
+                  borderRadius: 0,
+                  border: 1,
+                  fontSize: 0,
+                  marginBottom: 10,
+
+                  color:
+                    selectedGender === "female"
+                      ? "rgb(84, 176, 215)"
+                      : "rgb(84, 176, 215)",
+                }}>
+                <Female sx={{ fontSize: 42 }} />
+              </IconButton>
+
+              <IconButton
+                id="boy"
+                onClick={() => handleGenderClick("male")}
+                sx={{
+                  borderRadius: 0,
+                  border: 1,
+                  fontSize: 0,
+                  marginBottom: 10,
+
+                  color:
+                    selectedGender === "female"
+                      ? "rgb(84, 176, 215)"
+                      : "rgb(84, 176, 215)",
+                }}>
+                <Male sx={{ fontSize: 42 }} />
+              </IconButton>
             </div>
           </div>
 
@@ -40,20 +123,29 @@ function BMI() {
               <h3>Height: </h3>
               <h3>cm </h3>
             </div>
-            <input type="number"></input>
+            <input
+              value={height}
+              onChange={(event) => {
+                setHeight(event.target.value);
+              }}
+              type="number"
+            />
           </div>
-
 
           <div className="age">
             <div className="h33">
               <h3>Weight: </h3>
               <h3>hg </h3>
             </div>
-            <input type="number"></input>
+            <input
+              value={weight}
+              onChange={(event) => {
+                setWeight(event.target.value);
+              }}
+              type="number"
+            />
           </div>
         </div>
-
-        
       </div>
     </div>
   );
